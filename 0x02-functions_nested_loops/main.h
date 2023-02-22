3c894e520d357b46fabbc66666992f4d47d7a03d
Gorileng <keneilwe963@gmail.com>
2023-02-22 09:41:14 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef main.h
-#define main.h
+
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
   
-#endif
+
