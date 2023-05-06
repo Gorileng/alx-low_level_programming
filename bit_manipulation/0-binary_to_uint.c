a3f733a7decd71496b23dd50861fb2d5995de383
Gorileng <keneilwe963@gmail.com>
2023-05-06 02:19:33 -0700

    bit manipulation

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - unsigned int binary will be converted
+ *
+ * @a: Ptr to the string of characters
+ *
+ * Return: Converts the number
+ */
+
+unsigned int binary_to_uint(const char *a)
+{
+	signed int index = 0, num = 0;
+
+
+	if (!a)
+		return (0);
+
+	while (a[index] != '\0')
+	{
+		if (a[index] != '0' && a[index] != '1')
+		{
+			return (0);
+		}
+
+		num <<= 1;
+
+		if (a[index] & 1)
+		{
+			num += 1;
+		}
+		index += 1;
+	}
+	return (num);
+}
